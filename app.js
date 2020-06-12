df94e7dbd73768dd32ea171b712e49b7c8c1d184
Patrick Wong <saihowong81@hotmail.com>
2020-06-12 11:07:09 +0100

    added a comment

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
+// Start express appgit status
 const app = express();
 
 app.set('view engine', 'pug');
